7b8d379b4c7d6cf0a82790f3948cc1f256b21968
pr0fix <bgx152@myy.haaga-helia.fi>
2025-01-13 12:20:19 +0200

    add bg-color to resultpage and update screenshots

--- frontend/src/components/AnalysisResult.tsx
+++ frontend/src/components/AnalysisResult.tsx
@@ -12,18 +12,14 @@ const AnalysisResult = () => {
   const paragraphs = application.split("\n").filter((p) => p.trim());
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center bg-gray-100">
       <div className="mt-8 p-4 bg-white rounded-lg shadow-md w-3/4 max-w-3xl border">
         <h2 className="text-2xl font-semibold mb-4 text-gray-800">
           Tailored Application
         </h2>
         <div className="space-y-4">
           {paragraphs.map((paragraph, index) => (
-            <p
-              key={index}
-              className="text-gray-700 leading-relaxed"
-              
-            >
+            <p key={index} className="text-gray-700 leading-relaxed">
               {paragraph}
             </p>
           ))}
